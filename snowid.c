d33056ff92d3a8151d55aee982b77a3320bbbbdd
beyonddream <43626691+beyonddream@users.noreply.github.com>
2022-09-22 20:00:00 -0700

    * add snow state

--- snowid.c
+++ snowid.c
@@ -1,4 +1,21 @@
+
+#include <sys/time.h>
+#include <stdint.h>
+
 #include "snowid.h"
+
+typedef struct snow_state {
+    /**
+     * `checkpoint` is last time when an id was generated.
+     * If we detect current time is < checkpoint, then
+     * clock has moved backward and we refuse to generate
+     * the id.
+     */
+    struct timeval *checkpoint;
+    uint64_t worker_id;
+    uint16_t sequence_id;
+} snow_state;
+
 
 void init(snow_config *config)
 {
